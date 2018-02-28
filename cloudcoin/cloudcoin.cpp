721c11c04f9f312a522c4b921c985c7fe0a989c2
yao <myspiritcrazy@gmail.com>
2018-02-28 15:41:41 -0800

    code filter to detect contract name

--- cloudcoin/cloudcoin.cpp
+++ cloudcoin/cloudcoin.cpp
@@ -13,6 +13,12 @@ extern "C" {
     /// The apply method implements the dispatch of events to this contract
     void apply( uint64_t code, uint64_t action ) {
        eosio::print( "Hello World: ", eosio::name(code), "->", eosio::name(action), "\n" );
+
+        if (code == N(${contract_name})) {
+            //your handler to response to particular message
+            eosio::print( "Smart Contract: ", eosio::name(contrac_name), "\n" );
+        }
+
     }
 
 } // extern "C"
